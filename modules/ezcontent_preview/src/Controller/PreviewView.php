bf472db7a07a9a64fd7b4bd742c805810dcd9f90
Kuldev Singh <kuldev.singh@srijan.net>
2019-10-21 11:55:22 +0530

    adding preview module

---
+++ modules/ezcontent_preview/src/Controller/PreviewView.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\ezcontent_preview\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\NodeInterface;
+use Drupal\Component\Utility\UrlHelper;
+use Drupal\access_unpublished\AccessTokenManager;
+
+/**
+ * Preview the content
+ */
+class PreviewView extends ControllerBase {
+
+  public function preview(NodeInterface $node = NULL) {
+    $config = \Drupal::config('ezcontent_preview.settings');
+    $preview_base_url = $config->get('ezcontent_preview_url');
+    $node_id = $node->id();
+    $node_alias = \Drupal::service('path.alias_manager')->getAliasByPath('/node/' . $node_id);
+    $node_type = $node->getEntityType();
+    $url_query = '';
+    
+    // if node is unpublished using https://www.drupal.org/project/access_unpublished module
+    // generate token and pass it to Drupal
+    if(!$node->isPublished()) {
+      $tokenKey = \Drupal::config('access_unpublished.settings')->get('hash_key');
+      $tokenManager = \Drupal::service('access_unpublished.access_token_manager');
+      $activeToken = $tokenManager->getActiveAccessToken($node);
+      if(!$activeToken) {
+        $activeToken = $this->buildToken($node);
+      } 
+      $tokenValue = $activeToken->get('value')->value;
+
+      $urlQuery = UrlHelper::buildQuery(
+        [
+          $tokenKey => $tokenValue
+        ]
+      );
+      $urlQuery = '?' . UrlHelper::parse($urlQuery)['path'];
+    }
+
+    $output = '<iframe class="decoupled-content--preview" src="' . $preview_base_url . $node_alias . $url_query . '" width="100%"></iframe>';
+    return array(
+      '#type' => 'markup',
+      '#allowed_tags' => ['iframe'],
+      '#markup' => $output,
+    );
+
+  }
+
+  public function buildToken($entity) {
+    $tokenKey = \Drupal::config('ezcontent_preview.settings')->get('ezcontent_preview_token_expire_time');
+    if (!$tokenKey) {
+      $tokenKey = 300;
+    }
+    $access_token = \Drupal::entityTypeManager()->getStorage('access_token')->create(
+      [
+        'entity_type' => $entity->getEntityType()->id(),
+        'entity_id' => $entity->id(),
+        'expire' => \Drupal::time()->getRequestTime() + $tokenKey,
+      ]
+    );
+    $access_token->save();
+    return $access_token;
+  }
+
+}
