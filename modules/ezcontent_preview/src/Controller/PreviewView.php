53c5ec437a4fc5177fafd2a3be526c99207902a6
Kuldev Singh <kuldev.singh@srijan.net>
2019-11-14 18:34:45 +0530

    adding views integration, multiple configs

--- modules/ezcontent_preview/src/Controller/PreviewView.php
+++ modules/ezcontent_preview/src/Controller/PreviewView.php
@@ -5,6 +5,7 @@ namespace Drupal\ezcontent_preview\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\node\NodeInterface;
 use Drupal\ezcontent_preview\Utils;
+use Drupal\Core\Routing\TrustedRedirectResponse; 
 
 
 /**
@@ -12,23 +13,35 @@ use Drupal\ezcontent_preview\Utils;
  */
 class PreviewView extends ControllerBase {
 
-  public function preview(NodeInterface $node = NULL) {
-    $urlUtils = new Utils();
-    $url = $urlUtils->buildUrl($node);
-    if ($url) {
-      $output = '<iframe class="decoupled-content--preview" src="' . $url->toString() . '"></iframe>';
-      return array(
-        '#type' => 'markup',
-        '#allowed_tags' => ['iframe'],
-        '#markup' => $output,
-        '#attached' => [
-          'library' => [
-            'ezcontent_preview/global',
-          ],
-        ],
-      );
-    }
+  public function preview(NodeInterface $node = NULL, $preview_type) {
 
+    if ($preview_type) {
+      $decoupledRoutes = \Drupal::entityTypeManager()->getStorage('ezcontent_preview')->load($preview_type);
+      if($decoupledRoutes) {
+        $urlUtils = new Utils();
+        $url = $urlUtils->buildUrl($node, $decoupledRoutes);
+        if ($url) {
+
+          // if new tab open seperate a tab with decoupled URL
+          // else just iframe
+          if ($decoupledRoutes->newtab) {
+            return new TrustedRedirectResponse($url->toString());
+          }
+          $output = '<iframe class="decoupled-content--preview" src="' . $url->toString() . '"></iframe>';
+          return array(
+            '#type' => 'markup',
+            '#allowed_tags' => ['iframe'],
+            '#markup' => $output,
+            '#attached' => [
+              'library' => [
+                'ezcontent_preview/global',
+              ],
+            ],
+          );
+        }
+      }
+    }
+    
     // if nothing return as URL
     // return empty array
     return array(
