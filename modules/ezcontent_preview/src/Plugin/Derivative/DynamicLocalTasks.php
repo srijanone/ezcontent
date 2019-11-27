d752c03421bf107e8975e59f8a47f73d88fa5e32
Kuldev Singh <kuldev.singh@srijan.net>
2019-11-27 21:23:37 +0530

    adding custom access for local tasks

--- modules/ezcontent_preview/src/Plugin/Derivative/DynamicLocalTasks.php
+++ modules/ezcontent_preview/src/Plugin/Derivative/DynamicLocalTasks.php
@@ -17,34 +17,17 @@ class DynamicLocalTasks extends DeriverBase {
   public function getDerivativeDefinitions($base_plugin_definition) {
     $decoupledRoutes = \Drupal::entityTypeManager()->getStorage('ezcontent_preview')->loadMultiple();
     if ($decoupledRoutes) {
-      // @TODO better way to find get the node object
-      $current_path = \Drupal::service('path.current')->getPath();
-      $path_args = explode('/', $current_path);
-      if ($path_args && $path_args[1] == 'node') {
-        $node = \Drupal\node\Entity\Node::load($path_args[2]);
-        $node_type = $node->bundle();
-
-        foreach ($decoupledRoutes as $id => $route) {
-          $matched = FALSE;
-          foreach($route->content_entity as $entType) {
-            if ($entType === $node_type) {
-              $matched = TRUE;
-              break;
-            }
-          }
-          if ($matched) {
-            $target = ($route->newtab ? '_blank' : '_self');
-            $this->derivatives['ezcontent_preview.' . $id] = [
-              $base_plugin_definition,
-              'title' => $route->label,
-              'route_name' => 'ezcontent.preview.view',
-              'base_route' => 'entity.node.canonical',
-              'route_parameters' => ['preview_type' => $route->id],
-              'options' => ['attributes' => ['target' => $target]],
-              'weight' => 100,
-            ];
-          }
-        }
+      foreach ($decoupledRoutes as $id => $route) {
+        $target = ($route->newtab ? '_blank' : '_self');
+        $this->derivatives['ezcontent_preview.' . $id] = [
+          $base_plugin_definition,
+          'title' => $route->label,
+          'route_name' => 'ezcontent.preview.view',
+          'base_route' => 'entity.node.canonical',
+          'route_parameters' => ['preview_type' => $route->id],
+          'options' => ['attributes' => ['target' => $target]],
+          'weight' => 100,
+        ];
       }
     }
     return parent::getDerivativeDefinitions($base_plugin_definition);
