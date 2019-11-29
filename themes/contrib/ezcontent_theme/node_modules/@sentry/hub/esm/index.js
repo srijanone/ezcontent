5a7574a09c9b598ffde2e9f0f651e622cee236d3
sumit3021kumar <manudogra3021@gmail.com>
2019-11-29 11:36:49 +0530

    Removing node_module and css

--- themes/contrib/ezcontent_theme/node_modules/@sentry/hub/esm/index.js
+++
@@ -1,3 +0,0 @@
-export { addGlobalEventProcessor, Scope } from './scope';
-export { getCurrentHub, getHubFromCarrier, getMainCarrier, Hub, setHubOnCarrier } from './hub';
-//# sourceMappingURL=index.js.map
