3391295a6b182c4f6c4f582d3c5b80d5f9aed806
Mohammed Arshad Rizwan Ahmed <arshadkhan2631@gmail.com>
2020-04-15 11:07:21 +0530

    # This is a combination of 2 commits. # This is the 1st commit message:
    # This is a combination of 3 commits.
    # This is the 1st commit message:
    
    Added cards block type.
    
    Added functional unit test for profile.
    
    Removing phpunit.xml
    
    Added unit test case to test hex to rgb convert service.
    
    Removed un wanted files.
    
    Fixed format of the file.
    
    Added Referenced card block type.
    
    Removed job title field and renamed author field to source field in quote paragraph.
    
    Renamed generate summary to generate content.
    
    Added cards block type.
    
    Fixed one blank line issue.
    
    # This is the commit message #2:
    
    Added functional unit test for profile.
    
    Removing phpunit.xml
    
    # This is the commit message #3:
    
    Added unit test case to test hex to rgb convert service.
    
    Removed un wanted files.
    
    Fixed format of the file.
    
    # This is the commit message #2:
    
    Renamed generate summary to generate content.

---
+++ modules/ezcontent_paragraphs/modules/ezcontent_hero_media/tests/src/Unit/HexToRgbColorTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\Tests\ezcontent_hero_media\Unit;
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\ezcontent_hero_media\HexToRgba;
+
+/**
+ * @group ezcontent_smart_article
+ *
+ * @coversDefaultClass \Drupal\ezcontent_hero_media\HexToRgba
+ */
+class HexToRgbColorTest extends UnitTestCase {
+
+  /**
+   * @covers Drupal\ezcontent_hero_media\HexToRgba::hex2rgba
+   */
+  public function testHexToRgbColor() {
+    $hex_to_rgb = new HexToRgba();
+    $color = $hex_to_rgb->hex2rgba('#213c52', 0.37);
+    $this->assertEquals('rgba(33,60,82,0.37)', $color);
+  }
+}
