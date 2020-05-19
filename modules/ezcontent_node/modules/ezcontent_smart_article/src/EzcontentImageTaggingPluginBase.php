783bb1a97d55eb68db52a943ca7894e132047963
Mohammed Arshad Rizwan Ahmed <arshadkhan2631@gmail.com>
2020-05-19 18:55:24 +0530

    Fixed plugin base class issue of create function.

--- modules/ezcontent_node/modules/ezcontent_smart_article/src/EzcontentImageTaggingPluginBase.php
+++ modules/ezcontent_node/modules/ezcontent_smart_article/src/EzcontentImageTaggingPluginBase.php
@@ -3,12 +3,20 @@
 namespace Drupal\ezcontent_smart_article;
 
 use Drupal\Component\Plugin\PluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\file\Entity\File;
 
 /**
  * A base class implementation for image tagging plugin manager.
  */
 abstract class EzcontentImageTaggingPluginBase extends PluginBase implements EzcontentImageTaggingInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static($configuration, $plugin_id, $plugin_definition);
+  }
 
   /**
    * {@inheritdoc}
