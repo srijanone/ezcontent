52bff846994647d679c356d5ca55a3ce5787ffed
Mohammed Arshad Rizwan Ahmed <arshadkhan2631@gmail.com>
2021-02-08 16:18:26 +0530

    Fixing publish future issue.

--- modules/ezcontent_node/js/ezcontent_node_datetime.js
+++ modules/ezcontent_node/js/ezcontent_node_datetime.js
@@ -12,6 +12,27 @@
    */
   Drupal.behaviors.allowPastTimeForFutureDate = {
     attach: function (context) {
+      // Handle onload.
+      var publishedId = 'edit-publish-on-0-value-time';
+      var unpublishedId = 'edit-unpublish-on-0-value-time';
+
+      if ($('input#edit-publish-on-0-value-date').val() != drupalSettings.currentDate) {
+        $('#' + publishedId).attr('min', '00:00:00');
+      } else {
+        var dt = new Date();
+        var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+        $('#' + publishedId).attr('min', time);
+      }
+
+      if ($('input#edit-unpublish-on-0-value-date').val() != drupalSettings.currentDate) {
+        $('#' + unpublishedId).attr('min', '00:00:00');
+      } else {
+        var dt = new Date();
+        var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+        $('#' + unpublishedId).attr('min', time);
+      }
+
+      // Handle onchange.
       $('input#edit-publish-on-0-value-date , input#edit-unpublish-on-0-value-date', context).once().change(function () {
         var timeElementId = $(this).attr('id') === 'edit-publish-on-0-value-date'
           ? 'edit-publish-on-0-value-time' : 'edit-unpublish-on-0-value-time';
